9a0274f8d4385acf7978c96361af62e9f493a865
MSI\ryank <ryan.kertai@gmail.com>
2017-09-27 11:40:47 +1000

    Further work on the database pushing script, the clear() method now resets the entire database. Code is now commented and all the files required to complete the JSON generation are now added to the repo for reference.

--- Database Pushing Files/DataPushScript.py
+++ Database Pushing Files/DataPushScript.py
@@ -9,20 +9,21 @@ from firebase_admin import db
 # Import printing tools
 from pprint import pprint
 
+# Autheticating the program to make changes to the database
 cred = credentials.Certificate("sqn-981ba-firebase-adminsdk-bxk00-7dbb85d81f.json")
 firebase_admin.initialize_app(cred, {'databaseURL' : 'https://sqn-981ba.firebaseio.com'})
 
-def clear(): # Database Testing Service
-# Get a database reference
-    ref = db.reference('/0/Flights')
-    users_ref = ref.child('Person')
-    users_ref.set({
+def clear(): # This method resets the database
+    ref = db.reference('/Data') # Get the database reference
+    users_ref = ref.child('Person') # Create the table
+    users_ref.set({ # Populate the table with a key and fields under key
         'PMKeys': {
             'Name': ' ',
-            'id(Role)': ' '
+            'RoleID': ' '
             }
         })
-    ref = db.reference('/0/Flights')
+    # Rest of this method follows the above format for each table
+    ref = db.reference('/Data')
     users_ref = ref.child('Currency')
     users_ref.set({
         'CurrencyID': {
@@ -30,32 +31,72 @@ def clear(): # Database Testing Service
             'Period': ' '
             }
         })
+    ref = db.reference('/Data')
+    users_ref = ref.child('CurrencyRole')
+    users_ref.set({
+        'CurrencyID': {
+            'RoleID': ' ' 
+            }
+        })
+    ref = db.reference('/Data')
+    users_ref = ref.child('Flight')
+    users_ref.set({
+        'FlightID': {
+            'DTG': ' ',
+            'Landing': ' ',
+            'Platform': ' ',
+            'Takeoff': ' '
+            }
+        })
+    ref = db.reference('/Data')
+    users_ref = ref.child('PersonCurrency')
+    users_ref.set({
+        'PersonID': {
+            'CurrencyID': ' ',
+            'AchievementDate': ' '
+            }
+        })
+    ref = db.reference('/Data')
+    users_ref = ref.child('PersonFlight')
+    users_ref.set({
+        'FlightID': {
+            'PersonID': ' ',
+            'RoleID': ' '
+            }
+        })
+    ref = db.reference('/Data')
+    users_ref = ref.child('Role')
+    users_ref.set({
+        'RoleID': {
+            'RoleName': ' '
+            }
+        })
 
-def loadPersonData():
-    stopper = False
-    startPoint = 0
-    with open("PersonsTable.json") as json_file:
+def loadPersonData(): # This method loads all the data into the Person table from the created JSON
+    stopper = False # Used to halt the while loop once there are no more entries in the JSON
+    startPoint = 0 # Used to iterate through the JSON file
+    with open("PersonsTable.json") as json_file: # Open and load the JSON
         json_data = json.load(json_file)
     while(stopper == False):
         try:
-            pmkeys = (json_data["xml"]["items"]["item"][startPoint]["pmkeys"])
+            pmkeys = (json_data["xml"]["items"]["item"][startPoint]["pmkeys"]) # Storing the data within the JSON file
             name = (json_data["xml"]["items"]["item"][startPoint]["name"])
             role = (json_data["xml"]["items"]["item"][startPoint]["role"])
 
-            ref = db.reference('/0/Flights/Person')
-            posts_ref = ref.child(pmkeys)
-            posts_ref.set({
+            ref = db.reference('/Data/Person') # Creating the database reference
+            posts_ref = ref.child(pmkeys) # Creating a new entry within the Persons table with the Key of PMKeys   
+            posts_ref.set({ # Adding a name and role to the PMKeys reference
                 'Name' : name,
                 'id(Role)' : role
                 })
 
-            startPoint = startPoint + 1
-        except IndexError:
+            startPoint = startPoint + 1 # Used to iterate through the JSON
+        except IndexError: # This error is thrown upon reaching the end of the JSON
             print("---------------------------------------")
             print("------------END OF JSON----------------")
-            stopper = True
+            stopper = True # Ends the while loop
 
-def loadCurrencyData():
+def loadCurrencyData(): # This method works identically as the loadPersonData() method however loads currency data
     stopper = False
     startPoint = 0
     with open("CurrencyTable.json") as json_file:
@@ -66,7 +107,7 @@ def loadCurrencyData():
             currencyName = (json_data["xml"]["items"]["item"][startPoint]["currency"])
             period = (json_data["xml"]["items"]["item"][startPoint]["period"])
 
-            ref = db.reference('/0/Flights/Currency')
+            ref = db.reference('/Data/Currency')
             posts_ref = ref.child(currencyID)
             posts_ref.set({
                 'CurrencyName' : currencyName,
