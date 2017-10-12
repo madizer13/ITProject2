97e2b1f976723f712b84cf07a6b01cf87848e60d
MatthewPhelps <matthewphelps1@hotmail.com>
2017-10-12 23:57:02 +1100

    List Function Done
    Succesfullly listing all members in a button list. Needs to now call a funtion which only lists certain currencies for their role.

--- 34SQN/src/pages/updateCurrencies/updateCurrencies.ts
+++ 34SQN/src/pages/updateCurrencies/updateCurrencies.ts
@@ -5,7 +5,7 @@ import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
 import { HomePage } from './../home/home';
 //import { name } from 'ionic/dist';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController, Platform } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, Platform, AlertController, ActionSheetController } from 'ionic-angular';
 
 
 
@@ -18,29 +18,47 @@ templateUrl: 'updateCurrencies.html',
 
 })
 
-//CURRENTLY COPIED FROM THE SEARCH FUNCTION PAGE, MAKING CHANGES TO SEARCH CURRENCIES INSTEAD OF FLIGHTS
 export class UpdateCurrencies {
   public people: FirebaseListObservable<any>;
+  public selectedPerson: FirebaseObjectObservable<any>;
   constructor(public navCtrl: NavController,
         private modalCtrl: ModalController,
         public params: NavParams,
         private db: AngularFireDatabase,
         private platform: Platform,
+        public alertCtrl: AlertController,
+        public actionSheetCtrl: ActionSheetController
   ) {
-      this.getPeople();
+      this.people = db.list('/Data/Person/');
   }
 
-  getPeople(): FirebaseListObservable<any> {
-      return this.db.list('/Data/Person/', {
-          query: {
-              orderByChild: 'Name'
-          }
-      });
+  changeCurrencies(personID, personName)
+  {
+    let actionSheet = this.actionSheetCtrl.create({
+    title: 'What do you want to do?',
+    buttons: [
+      {
+        text: 'View LogBook',
+        role: 'destructive',
+        handler: () => {
+          console.log("done1");
+        }
+      },{
+        text: 'Update Currencies',
+        handler: () => {
+          console.log("done2");
+        }
+      },{
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }
+    ]
+  });
+  actionSheet.present();
   }
-
-
-
-
 goBack(): void {
      this.navCtrl.setRoot(HomePage);
 }
