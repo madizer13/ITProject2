72dc9d3ee26fe748ffb6353456dfdb1d966c98db
madizer13 <z5056954@student.unsw.edu.au>
2017-10-15 19:53:47 +1100

    View/Edit Currenices
    Can navigate to the view/edit currencies page from searching a person,
    backend is still being developed to pull currencies from database
    however GUI is done.

--- 34SQN/src/pages/updateCurrencies/updateCurrencies.ts
+++ 34SQN/src/pages/updateCurrencies/updateCurrencies.ts
@@ -15,50 +15,15 @@ import { IonicPage, NavController, NavParams, ModalController, Platform } from '
 @Component({
     selector: 'page-updateCurrencies',
     templateUrl: 'updateCurrencies.html',
+})
 
-    })
-
-    //CURRENTLY COPIED FROM THE SEARCH FUNCTION PAGE, MAKING CHANGES TO SEARCH CURRENCIES INSTEAD OF FLIGHTS
 export class UpdateCurrencies {
-    public Flight: FirebaseListObservable<any[]>;
-    title: any;
-
-    constructor(public navCtrl: NavController,
-        private modalCtrl: ModalController,
-        public params: NavParams,
-        private databa: AngularFireDatabase,
-        private platform: Platform,
-    ) {
-        this.Flight = databa.list('/random');
-
-        if (params.get('isEdited')) {
-            let flight = params.get('flight'),
-                Flight = {
-                    name: 'flight',
-                    date: 'Select date',
-                    duration: 'hours'
-                };
-        }
-
-        var ref = firebase.database().ref("flight");
-        //ref.orderByKey().endAt("23").on("child_added"), function (snapshot){
-        //console.log(snapshot.key);
-        // };
-
-    }
-
-    ionViewDidLoad() {
-        console.log('ionViewDidLoad SearchPage');
-        this.platform.ready()
-            .then(() => {
-                this.Flight = this.databa.list('/title');
-            });
-
-    }
- 
+    constructor(public navCtrl: NavController) { }
 
     goBack(): void {
         this.navCtrl.setRoot(HomePage);
     }
+
+
 
 }
