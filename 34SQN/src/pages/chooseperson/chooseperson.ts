4e74358bed662cebfc897453c13852485f0a39c2
Matthew Phelps <matthewphelps1@hotmail.com>
2017-10-20 22:15:28 +1100

    Daily Save
    Saving a copy for use on my laptop. Am trying to develop updateCurrencies and a search bar on choosePerson.

--- 34SQN/src/pages/chooseperson/chooseperson.ts
+++ 34SQN/src/pages/chooseperson/chooseperson.ts
@@ -7,63 +7,83 @@ import { HomePage } from './../home/home';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController, Platform, AlertController, ActionSheetController } from 'ionic-angular';
 
-
-
 @IonicPage({
-name: 'chooseperson'
+  name: 'chooseperson'
 })
 @Component({
-selector: 'page-chooseperson',
-templateUrl: 'chooseperson.html',
+  selector: 'page-chooseperson',
+  templateUrl: 'chooseperson.html',
 
 })
 
 export class Chooseperson {
   public people: FirebaseListObservable<any>;
   constructor(public navCtrl: NavController,
-        private modalCtrl: ModalController,
-        public params: NavParams,
-        private db: AngularFireDatabase,
-        private platform: Platform,
-        public alertCtrl: AlertController,
-        public actionSheetCtrl: ActionSheetController
+    private modalCtrl: ModalController,
+    public params: NavParams,
+    private db: AngularFireDatabase,
+    private platform: Platform,
+    public alertCtrl: AlertController,
+    public actionSheetCtrl: ActionSheetController
   ) {
-      this.people = db.list('/Data/Person/');
+    this.initializePeople();
   }
 
- selectOption(personKey, personName, personRole)
-  {
+  selectOption(personKey, personName, personRole) {
     let actionSheet = this.actionSheetCtrl.create({
-    title: 'What do you want to do for ' + personName + '?',
-    buttons: [
-      {
-        text: 'View LogBook',
-        role: 'destructive',
-        handler: () => {
-          console.log("done1");
+      title: 'What do you want to do for ' + personName + '?',
+      buttons: [
+        {
+          text: 'View LogBook',
+          role: 'destructive',
+          handler: () => {
+            console.log("done1");
+          }
+        }, {
+          text: 'View Currencies',
+          handler: () => {
+            this.updateCurrencies();
+          }
+        }, {
+          text: 'Update Currencies',
+          handler: () => {
+            console.log("done2");
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
         }
-      },{
-        text: 'View Currencies',
-        handler: () => {
-          console.log("done3");
-        }
-      },{
-        text: 'Update Currencies',
-        handler: () => {
-          console.log("done2");
-        }
-      },{
-        text: 'Cancel',
-        role: 'cancel',
-        handler: () => {
-          console.log('Cancel clicked');
-        }
-      }
-    ]
-  });
-  actionSheet.present();
+      ]
+    });
+    actionSheet.present();
   }
-goBack(): void {
-     this.navCtrl.setRoot(HomePage);
-}
+
+  initializePeople(){
+    this.people = this.db.list('/Data/Person/');
+  }
+
+  getPeople(ev: any) {
+    this.initializePeople();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.people = this.people.filter((item) => {
+        return (this.people.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+  updateCurrencies(){
+    let nav = this.navCtrl.setRoot('updateCurrencies');
+  }
+
+  goBack():void{
+    this.navCtrl.setRoot(HomePage);
+  }
 }
